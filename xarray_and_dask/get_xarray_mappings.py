ff3ba8b99030c5d9fcf4af5ab67db339a84702a4
Lynne Merchant <lmerchant@ucsd.edu>
2022-01-14 02:42:42 -0800

    Add netcdf name to name mappings and select c_format attr

--- xarray_and_dask/get_xarray_mappings.py
+++ xarray_and_dask/get_xarray_mappings.py
@@ -1,88 +1,5 @@
 
-def get_argovis_mappings_param(nc):
-
-    param_mapping = {}
-
-    param_units = {}
-    param_ref_scale = {}
-    param_c_format = {}
-    param_dtype = {}
-
-    # Param: Save units, ref_scale, and c_format, dtype
-
-    for var in nc.keys():
-        try:
-            param_units[var] = nc[var].attrs['units']
-        except KeyError:
-            pass
-
-        try:
-            if nc[var].attrs['reference_scale'] != 'unknown':
-                param_ref_scale[var] = nc[var].attrs['reference_scale']
-        except KeyError:
-            pass
-
-        try:
-            param_c_format[var] = nc[var].attrs['C_format']
-        except KeyError:
-            pass
-
-        try:
-            param_dtype[var] = nc[var].dtype
-        except KeyError:
-            pass
-
-    param_mapping['names'] = list(nc.keys())
-    param_mapping['units'] = param_units
-    param_mapping['ref_scale'] = param_ref_scale
-    param_mapping['c_format'] = param_c_format
-    param_mapping['dtype'] = param_dtype
-
-    return param_mapping
-
-
-def get_argovis_mappings_meta(nc):
-
-    meta_mapping = {}
-
-    meta_units = {}
-    meta_ref_scale = {}
-    meta_c_format = {}
-    meta_dtype = {}
-
-    # Meta: Save units, ref_scale, c_format, dtype
-    for var in nc.coords:
-
-        try:
-            meta_units[var] = nc[var].attrs['units']
-        except KeyError:
-            pass
-
-        try:
-            meta_ref_scale[var] = nc[var].attrs['reference_scale']
-        except KeyError:
-            pass
-
-        try:
-            meta_c_format[var] = nc[var].attrs['C_format']
-        except KeyError:
-            pass
-
-        try:
-            meta_dtype[var] = nc[var].dtype
-        except KeyError:
-            pass
-
-    meta_mapping['names'] = list(nc.coords)
-    meta_mapping['units'] = meta_units
-    meta_mapping['ref_scale'] = meta_ref_scale
-    meta_mapping['c_format'] = meta_c_format
-    meta_mapping['dtype'] = meta_dtype
-
-    return meta_mapping
-
-
-# def get_nc_variable_mappings_param(nc, param_var_mapping):
+# def get_argovis_mappings_param(nc):
 
 #     param_mapping = {}
 
@@ -124,6 +41,47 @@ def get_argovis_mappings_meta(nc):
 #     return param_mapping
 
 
+# def get_argovis_mappings_meta(nc):
+
+#     meta_mapping = {}
+
+#     meta_units = {}
+#     meta_ref_scale = {}
+#     meta_c_format = {}
+#     meta_dtype = {}
+
+#     # Meta: Save units, ref_scale, c_format, dtype
+#     for var in nc.coords:
+
+#         try:
+#             meta_units[var] = nc[var].attrs['units']
+#         except KeyError:
+#             pass
+
+#         try:
+#             meta_ref_scale[var] = nc[var].attrs['reference_scale']
+#         except KeyError:
+#             pass
+
+#         try:
+#             meta_c_format[var] = nc[var].attrs['C_format']
+#         except KeyError:
+#             pass
+
+#         try:
+#             meta_dtype[var] = nc[var].dtype
+#         except KeyError:
+#             pass
+
+#     meta_mapping['names'] = list(nc.coords)
+#     meta_mapping['units'] = meta_units
+#     meta_mapping['ref_scale'] = meta_ref_scale
+#     meta_mapping['c_format'] = meta_c_format
+#     meta_mapping['dtype'] = meta_dtype
+
+#     return meta_mapping
+
+
 def get_var_mapping(nc, var_names):
 
     # TODO
@@ -137,6 +95,7 @@ def get_var_mapping(nc, var_names):
     var_ref_scale = {}
     var_c_format = {}
     var_dtype = {}
+    var_netcdf_name = {}
 
     for var in var_names:
 
@@ -151,8 +110,13 @@ def get_var_mapping(nc, var_names):
         except KeyError:
             pass
 
+        if 'source_C_format' in nc[var].attrs:
+            c_format_attr = 'source_C_format'
+        else:
+            c_format_attr = 'C_format'
+
         try:
-            var_c_format[var] = nc[var].attrs['C_format']
+            var_c_format[var] = nc[var].attrs[c_format_attr]
         except KeyError:
             pass
 
@@ -161,10 +125,16 @@ def get_var_mapping(nc, var_names):
         except KeyError:
             pass
 
+        try:
+            var_netcdf_name[var] = nc[var].attrs['standard_name']
+        except KeyError:
+            pass
+
     mapping['units'] = var_units
     mapping['ref_scale'] = var_ref_scale
     mapping['c_format'] = var_c_format
     mapping['dtype'] = var_dtype
+    mapping['netcdf_name'] = var_netcdf_name
 
     return mapping
 
@@ -172,12 +142,13 @@ def get_var_mapping(nc, var_names):
 def get_nc_variable_mappings(nc, coords_vars_names):
 
     # get names, units, ref_scale, c_format, dtype
+    # and netcdf standard_name attribute
 
     coords_vars_mapping = get_var_mapping(nc, coords_vars_names)
 
     mapping = {}
 
-    keys = ['units', 'ref_scale', 'c_format', 'dtype']
+    keys = ['units', 'ref_scale', 'c_format', 'dtype', 'netcdf_name']
 
     for key in keys:
         mapping[key] = coords_vars_mapping[key]
@@ -185,26 +156,3 @@ def get_nc_variable_mappings(nc, coords_vars_names):
     mapping['names'] = coords_vars_names
 
     return mapping
-
-# def get_nc_variable_mappings_orig(nc, coords_vars_mapping):
-
-#     # get names, units, ref_scale, c_format, dtype
-
-#     coords_names = coords_vars_mapping['coords']
-#     coords_mapping = get_var_mapping(nc, coords_names)
-
-#     vars_names = coords_vars_mapping['vars']
-#     vars_mapping = get_var_mapping(nc, vars_names)
-
-#     mapping = {}
-
-#     keys = ['units', 'ref_scale', 'c_format', 'dtype']
-
-#     for key in keys:
-#         mapping[key] = {**coords_mapping[key], **vars_mapping[key]}
-
-#     all_names = [*coords_names, *vars_names]
-
-#     mapping['names'] = all_names
-
-#     return mapping
