c86330b5baa9c1533ffe97547f8b6028ec38e036
Lynne Merchant <lmerchant@ucsd.edu>
2022-01-14 03:05:15 -0800

    Remove commented out code and add func to save single source profiles

--- check_and_save/save_output.py
+++ check_and_save/save_output.py
@@ -7,10 +7,8 @@ from pathlib import Path
 import logging
 
 from global_vars import GlobalVars
-#from check_and_save.check_of_ctd_vars import check_of_ctd_vars
-from create_profiles.create_meas_profiles import filter_measurements
-from variable_mapping.meta_param_mapping import get_mappings_keys_mapping
-from check_and_save.save_as_zip import save_as_zip
+
+from variable_mapping.meta_param_mapping import get_program_argovis_mapping
 from check_and_save.save_as_zip import save_as_zip_data_type_profiles
 
 
@@ -32,7 +30,7 @@ def get_unique_cchdo_units(data_type_profiles):
     # need this since already renamed individual files
 
     # cchdo names are the keys and argovis names are the value
-    key_mapping = get_mappings_keys_mapping()
+    key_mapping = get_program_argovis_mapping()
 
     cchdo_units_key = 'cchdoUnits'
     renamed_cchdo_units_key = key_mapping[cchdo_units_key]
@@ -105,7 +103,7 @@ def write_profile_cchdo_units_one_profile(data_type, profile):
     # keep a record of what units need to be converted
 
     # cchdo names are the keys and argovis names are the value
-    key_mapping = get_mappings_keys_mapping()
+    key_mapping = get_program_argovis_mapping()
 
     cchdo_units_key = 'cchdoUnits'
     renamed_cchdo_units_key = key_mapping[cchdo_units_key]
@@ -142,7 +140,7 @@ def write_profile_cchdo_units(checked_profiles_info):
     # need this since already renamed individual files
 
     # cchdo names are the keys and argovis names are the value
-    key_mapping = get_mappings_keys_mapping()
+    key_mapping = get_program_argovis_mapping()
 
     cchdo_units_key = 'cchdoUnits'
     renamed_cchdo_units_key = key_mapping[cchdo_units_key]
@@ -261,32 +259,6 @@ def write_profile_json(profile_dict):
                   sort_keys=False, default=convert)
 
 
-# def save_one_btl_ctd_profile(ctd_var_check):
-
-#     has_all_ctd_vars = ctd_var_check['has_all_ctd_vars']
-
-#     profile = ctd_var_check['profile_checked']
-
-#     profile_dict = profile['profile_dict']
-
-#     if has_all_ctd_vars['btl'] or has_all_ctd_vars['ctd']:
-#         write_profile_json(profile_dict)
-
-
-# def save_all_btl_ctd_profiles(checked_profiles_info):
-
-#     # checked_profiles_info holds info for all profiles
-#     for checked_profile_info in checked_profiles_info:
-
-#         has_all_ctd_vars = checked_profile_info['has_all_ctd_vars']
-
-#         profile = checked_profile_info['profile_checked']
-#         profile_dict = profile['profile_dict']
-
-#         if has_all_ctd_vars['btl'] or has_all_ctd_vars['ctd']:
-#             write_profile_json(profile_dict)
-
-
 def save_included_excluded_cchdo_vars(included, excluded):
     """
         Save included vars
@@ -330,141 +302,6 @@ def save_included_excluded_cchdo_vars(included, excluded):
                 f.write(f"{id}\n")
 
 
-# def save_profile_one_type(checked_profile_info):
-
-#     has_all_ctd_vars = checked_profile_info['has_all_ctd_vars']
-#     data_type = checked_profile_info['data_type']
-
-#     profile = checked_profile_info['profile_checked']
-#     profile_dict = profile['profile_dict']
-#     expocode = profile_dict['meta']['expocode']
-
-#     # add_vars_to_included_excluded_collections(
-#     #     profile_dict, cruises_collections)
-
-#     if has_all_ctd_vars[data_type]:
-#         write_profile_json(profile_dict)
-
-
-# def save_all_profiles_one_type(checked_profiles_info):
-
-#     for checked_profile_info in checked_profiles_info:
-#         save_profile_one_type(checked_profile_info)
-
-
-# def check_and_save_per_type(data_type_obj_profiles):
-
-#     # Now check if profiles have CTD vars and should be saved
-#     # And filter btl and ctd measurements separately
-
-#     data_type = data_type_obj_profiles['data_type']
-#     data_type_profiles = data_type_obj_profiles['profiles']
-
-#     # filter measurements for core using hierarchy
-#     data_type_profiles = filter_measurements(data_type_profiles, data_type)
-
-#     # Already filtered measurements, but didn't rename
-#     # salinity to psal in case needed to know saliniity
-#     # existed and could be used
-
-#     # checked_profiles_info is for combo of data types
-#     checked_profiles_info, ctd_vars_flag = check_of_ctd_vars(
-#         data_type_profiles)
-
-#     if ctd_vars_flag:
-#         logging.info('----------------------')
-#         logging.info('Saving files')
-#         logging.info('----------------------')
-
-#         write_profile_cchdo_units(checked_profiles_info)
-
-#         save_as_zip(checked_profiles_info)
-
-#         # save_all_profiles_one_type(checked_profiles_info)
-
-#     else:
-#         logging.info("*** Cruise not converted ***")
-
-#         profile = data_type_profiles[0]
-#         cruise_expocode = profile['profile_dict']['meta']['expocode']
-
-#         filename = 'cruises_not_converted.txt'
-#         filepath = os.path.join(GlobalVars.LOGGING_DIR, filename)
-#         with open(filepath, 'a') as f:
-#             f.write(f"{cruise_expocode}\n")
-
-
-# def check_and_save_combined(profiles_btl_ctd):
-
-#     logging.info("inside check_and_save_combined")
-
-#     # Now check if profiles have CTD vars and should be saved
-#     checked_profiles_info, ctd_vars_flag = check_of_ctd_vars(
-#         profiles_btl_ctd)
-
-#     if ctd_vars_flag:
-#         logging.info('----------------------')
-#         logging.info('Saving files')
-#         logging.info('----------------------')
-
-#         write_profile_cchdo_units(checked_profiles_info)
-
-#         save_as_zip(checked_profiles_info)
-
-#         # save_all_btl_ctd_profiles(checked_profiles_info)
-
-#     else:
-#         logging.info("*** Cruise not converted ***")
-
-#         profile = profiles_btl_ctd[0]
-#         cruise_expocode = profile['profile_dict']['meta']['expocode']
-
-#         filename = 'cruises_not_converted.txt'
-#         filepath = os.path.join(GlobalVars.LOGGING_DIR, filename)
-#         with open(filepath, 'a') as f:
-#             f.write(f"{cruise_expocode}\n")
-
-
-# def rename_salinity_to_psal(measurements):
-
-#     new_measurements = []
-#     for obj in measurements:
-#         if 'salinity' in obj.keys():
-#             obj['psal'] = obj.pop('salinity')
-
-#     return new_measurements
-
-
-# def rename_salinity_col(data_type_profiles, type):
-
-#     # For measurements, already filtered whether to
-#     # keep psal or salinity, but didn't rename the
-#     # salinity col. Do that here
-
-#     output_profiles_list = []
-
-#     for profile in data_type_profiles:
-
-#         profile_dict = profile['profile_dict']
-#         station_cast = profile['station_cast']
-
-#         measurements = profile_dict['measurements']
-
-#         for obj in measurements:
-#             if 'salinity' in obj.keys():
-#                 obj['psal'] = obj.pop('salinity')
-
-#         profile_dict['measurements'] = measurements
-
-#         output_profile = {}
-#         output_profile['profile_dict'] = profile_dict
-#         output_profile['station_cast'] = station_cast
-
-#         output_profiles_list.append(output_profile)
-
-#     return output_profiles_list
-
-
 def save_data_type_profiles(data_type_obj_profiles):
 
     logging.info('Saving files single type')
@@ -473,18 +310,25 @@ def save_data_type_profiles(data_type_obj_profiles):
 
     data_type_profiles = data_type_obj_profiles['data_type_profiles_list']
 
-    # Set measurements = [] if all vals null besides pres
-    # I already accounted for this
-    #data_type_profiles = filter_measurements(data_type_profiles)
-
-    # TODO
     # Look at one profile to get the cchdo units
-    # assuming same for all profiles. Is this true?
+    # to see what kinds there are
     all_cchdo_units_mapping = get_unique_cchdo_units(data_type_profiles)
 
     write_all_cchdo_units(all_cchdo_units_mapping)
 
     save_as_zip_data_type_profiles(data_type_profiles)
+
+
+def save_data_type_profiles_per_type(single_data_type_cruises):
+
+    # TODO test this part with expocode of one data type
+    for cruise_obj in single_data_type_cruises:
+
+        all_data_types_profile_objs = cruise_obj['all_data_types_profile_objs']
+
+        for data_type_obj_profiles in all_data_types_profile_objs:
+            # Remove any measurements with temp = NaN when saving
+            save_data_type_profiles(data_type_obj_profiles)
 
 
 def save_data_type_profiles_combined(combined_obj_profiles):
