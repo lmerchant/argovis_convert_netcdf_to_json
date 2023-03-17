3add072567bcf3dd4d4d26abb4f0a2bd11f8120c
Lynne Merchant <lmerchant@ucsd.edu>
2023-03-17 05:06:52 -0700

    Remove checking if both ctd and btl in profile

--- process_cruises/post_process_cruise_objs_by_collection.py
+++ process_cruises/post_process_cruise_objs_by_collection.py
@@ -37,49 +37,10 @@ def post_process_cruise_objs_by_collection(cruise_objs_by_type):
         with open(processed_cruises_file, 'a') as f:
             f.write(f"{expocode}\n")
 
-        # In profiles objs, the combined type may just
-        # be btl and not ctd or ctd and not btl
-        # but within all the profiles, if find a btl and ctd
-        # apply combined function
+        # For meta data, add meta with data_type suffix removed
+        all_profiles = update_profiles_single_type(
+            all_data_types_profile_objs)
 
-        is_btl = any([True if profiles_obj['data_type'] ==
-                      'btl' else False for profiles_obj in all_data_types_profile_objs])
-
-        is_ctd = any([True if profiles_obj['data_type'] ==
-                      'ctd' else False for profiles_obj in all_data_types_profile_objs])
-
-        if is_btl and is_ctd:
-
-            logging.info("Combining btl and ctd")
-
-            # But for key source_info['instrument'], shouldn't
-            # just be a global value of 'ship_ctd_btl' because
-            # sometimes it's a combined cruise, but a data type profile may
-            # not be at that station cast
-
-            # filter measurements by hierarchy
-            #  when combine btl and ctd profiles.
-            # didn't filter btl or ctd first in case need
-            # a variable from both
-
-            # Each obj in all_data_types_profile_objs is a
-            # dict with keys station_cast, data_type, profile_dict
-            all_profiles = create_profiles_combined_type(
-                all_data_types_profile_objs)
-
-            save_data_type_profiles(all_profiles)
-
-        else:
-
-            if is_btl:
-                data_type = 'btl'
-            elif is_ctd:
-                data_type = 'ctd'
-
-            # For meta data, add meta with data_type suffix removed
-            all_profiles = update_profiles_single_type(
-                all_data_types_profile_objs, data_type)
-
-            # Inside save, if not btl_ctd data type, will filter out
-            # measurements objects with temp = NaN
-            save_data_type_profiles(all_profiles)
+        # Inside save, if not btl_ctd data type, will filter out
+        # measurements objects with temp = NaN
+        save_data_type_profiles(all_profiles)
