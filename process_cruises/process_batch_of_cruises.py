f2f5a9e2468038dc0821ca1c0310539e1f130eaa
Lynne Merchant <lmerchant@ucsd.edu>
2023-03-18 17:57:51 -0700

    Update comments

--- process_cruises/process_batch_of_cruises.py
+++ process_cruises/process_batch_of_cruises.py
@@ -4,7 +4,6 @@ from process_cruises.process_cruise_objs_by_type import process_cruise_objs_by_t
 from process_cruises.post_process_cruise_objs_by_type import post_process_cruise_objs_by_type
 from process_cruises.post_process_cruise_objs_by_collection import post_process_cruise_objs_by_collection
 
-#from check_and_save.save_output import save_data_type_profiles_per_type
 from check_and_save.add_vars_to_logged_collections import gather_included_excluded_vars
 from check_and_save.save_output import save_included_excluded_cchdo_vars
 
@@ -25,32 +24,21 @@ def process_batch_of_cruises(cruise_objs):
 
     cruise_objs_by_type = process_cruise_objs_by_type(cruise_objs)
 
+    # Uncomment the following to see format of one profile after it has been processed
     # one_cruise = cruise_objs_by_type[0]
     # type_objs = one_cruise['all_data_types_profile_objs']
-
     # one_type = type_objs[0]
-
     # profiles = one_type['data_type_profiles_list']
-
     # one_profile = profiles[1]
-
     # print(one_profile['profile_dict']['cchdo_param_names'])
-
     # print(f"\n")
-
     # print(one_profile)
-
     # exit(1)
 
     # **********************************
-    # Rename variables to ArgoVis names
-    # (add suffix and rename core)
-    # And create mapping keys for Argovis values
+    # Post process cruise objs by type
     # **********************************
 
-    # Load dict back into pandas dataframe to rename and then
-    # save back as json.
-    #
     # TODO
     # Make sure the precision didn't change
     # from the c_format
@@ -60,33 +48,30 @@ def process_batch_of_cruises(cruise_objs):
     # TODO
     # Should be throwing out variables with all NaN values before
     # deciding how to name things.
-    # For example, a btl file dataset can have both oxygen and
-    # ctd oxygen, but if ctd oxygen is all Nan, it's not renaming
-    # oxygen to doxy like it should be.
 
-    cruise_objs_by_type = post_process_cruise_objs_by_type(
-        cruise_objs_by_type)
+    # For example, a btl file dataset can have both oxygen and
+    # ctd oxygen, but what if ctd oxygen is all Nan, it won't instead
+    # use the oxygen variable and rename it to doxy
+
+    cruise_objs_by_type = post_process_cruise_objs_by_type(cruise_objs_by_type)
+
+    # ***********************************************
+    # Post process cruise objs by collection
+    # ************************************************
 
     post_process_cruise_objs_by_collection(cruise_objs_by_type)
 
+    # Uncomment the following to see format of one profile after it has been processed
     # one_cruise = cruise_objs_by_type[0]
     # type_objs = one_cruise['all_data_types_profile_objs']
-
     # print(f"total objs = {len(type_objs)}")
-
     # # one_type = type_objs[0] # ('get btl if have btl_ctd type)
     # one_type = type_objs[1]  # (get ctd if have btl_ctd type)
-
     # profiles = one_type['data_type_profiles_list']
-
-    # one_profile = profiles[15]
-
+    # one_profile = profiles[0]
     # print(one_profile['profile_dict']['data_keys'])
-
     # print(f"\n")
-
     # print(one_profile)
-
     # exit(1)
 
     # ***********************************
@@ -100,30 +85,6 @@ def process_batch_of_cruises(cruise_objs):
     cruises_all_included, cruises_all_excluded = gather_included_excluded_vars(
         cruise_objs_by_type)
 
-    # one_cruise = cruise_objs_by_type[0]
-    # type_objs = one_cruise['all_data_types_profile_objs']
-
-    # one_type = type_objs[0]
-
-    # profiles = one_type['data_type_profiles_list']
-
-    # one_profile = profiles[0]
-
-    # print(one_profile['profile_dict']['meta']['source'])
-
-    # exit(1)
-
     # Rename included and excluded vars and save
     save_included_excluded_cchdo_vars(
         cruises_all_included, cruises_all_excluded)
-
-    # ***********************************************
-    # Post process cruise objs by collection
-    # ************************************************
-
-    logging.info('-----------------------------------------')
-    logging.info("Filter out single data type to save next")
-    logging.info("Update profiles")
-    logging.info('-----------------------------------------')
-
-    # post_process_cruise_objs_by_collection(cruise_objs_by_type)
