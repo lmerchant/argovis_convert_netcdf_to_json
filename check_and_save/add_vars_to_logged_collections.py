1c7b05bb1c9b18b720d2cb408d42bcd4b236cffb
Lynne Merchant <lmerchant@ucsd.edu>
2023-06-02 05:19:34 -0700

    Change how key data_info is accessed

--- check_and_save/add_vars_to_logged_collections.py
+++ check_and_save/add_vars_to_logged_collections.py
@@ -3,7 +3,6 @@ import logging
 
 
 def get_argovis_param_names(profile_dict):
-
     argovis_param_names = []
 
     profile_keys = profile_dict.keys()
@@ -14,7 +13,7 @@ def get_argovis_param_names(profile_dict):
     # cchdo names are the keys and argovis names are the value
     key_mapping = get_program_argovis_data_info_mapping()
 
-    param_names_key = 'argovis_param_names'
+    param_names_key = "argovis_param_names"
     renamed_param_names_key = key_mapping(param_names_key)
 
     # For param data
@@ -42,14 +41,13 @@ def get_argovis_param_names(profile_dict):
 
 
 def add_argovis_vars_one_profile(profile_dict):
-
     # TODO
     # Use my names and map to latest argovis names
     # So since renamed single types already, need to use the argovis names
 
-    profile_id = profile_dict['meta']['_id']
+    profile_id = profile_dict["meta"]["_id"]
 
-    data_type = profile_dict['data_type']
+    data_type = profile_dict["data_type"]
 
     # # Map back to cchdo names
     # cchdo_meta_keys = rn.convert_argovis_meta_to_cchdo_names(
@@ -67,43 +65,41 @@ def add_argovis_vars_one_profile(profile_dict):
 
     # cchdo names are the keys and argovis names are the value
     # Find what the new key name is
-    cchdo_key = 'cchdo_argovis_param_mapping'
+    cchdo_key = "cchdo_argovis_param_mapping"
 
-    key_mapping = get_program_argovis_data_info_mapping()
+    # key_mapping = get_program_argovis_data_info_mapping()
 
-    renamed_key = key_mapping[cchdo_key]
+    # renamed_key = key_mapping[cchdo_key]
 
     # Mapping of all the CCHDO names to argovis names with data_type suffix
     # TODO
     # Check these mappings include the data type suffix
-    #data_info = profile_dict['meta'][f"data_info_{data_type}"]
-    data_info = profile_dict['meta']['data_info']
-    name_mapping = data_info[renamed_key]
+    data_info = profile_dict["meta"]["data_info"][0]
 
     # Any names found in the name mapping have been included in the profile data
     # because any that were all empty were dropped before here
-    included_param_argovis_names = list(name_mapping.values())
+    # included_param_argovis_names = list(name_mapping.values())
+    included_param_argovis_names = data_info[0]
 
     # Get Param names before filtering out empty cols
     # These are the names in original cchdo file which includes
     # empty columns due to starting netcdf file containing all
     # variable names for all the profiles
 
-    cchdo_key = 'argovis_param_names'
+    # cchdo_key = "argovis_param_names"
 
-    key_mapping = get_program_argovis_data_info_mapping()
+    # key_mapping = get_program_argovis_data_info_mapping()
 
-    renamed_argovis_param_names_key = key_mapping[cchdo_key]
+    data_info = profile_dict["meta"]["data_info"][0]
 
-    data_info = profile_dict['meta']['data_info']
-
-    argovis_param_names = data_info[renamed_argovis_param_names_key]
+    argovis_param_names = data_info[0]
 
     included_argovis_param_names_set = set(included_param_argovis_names)
     argovis_param_names_set = set(argovis_param_names)
 
     excluded_argovis_param_names = argovis_param_names_set.difference(
-        included_argovis_param_names_set)
+        included_argovis_param_names_set
+    )
 
     # *******************************
     # Save included and excluded vars
@@ -114,7 +110,7 @@ def add_argovis_vars_one_profile(profile_dict):
     # Add tuple (argovis_name, profile_id, data_type)
     # ***********************************
 
-    data_type = profile_dict['data_type']
+    data_type = profile_dict["data_type"]
 
     for name in included_param_argovis_names:
         included.append((name, profile_id, data_type))
@@ -122,11 +118,15 @@ def add_argovis_vars_one_profile(profile_dict):
     for name in excluded_argovis_param_names:
         excluded.append((name, profile_id, data_type))
 
-    return included, excluded, included_param_argovis_names, excluded_argovis_param_names
+    return (
+        included,
+        excluded,
+        included_param_argovis_names,
+        excluded_argovis_param_names,
+    )
 
 
 def add_vars_one_cruise(data_type_profiles_objs):
-
     vars_included = []
     vars_excluded = []
 
@@ -135,20 +135,22 @@ def add_vars_one_cruise(data_type_profiles_objs):
 
     # Loop over the profiles for the collection of files that have profiles
     for data_type_profiles_obj in data_type_profiles_objs:
-
-        data_type_profiles = data_type_profiles_obj['data_type_profiles_list']
+        data_type_profiles = data_type_profiles_obj["data_type_profiles_list"]
 
         for data_type_profile in data_type_profiles:
-
-            profile_dict = data_type_profile['profile_dict']
+            profile_dict = data_type_profile["profile_dict"]
 
             # included and excluded are tuples with profile_id
 
             # included_param_cchdo_names and excluded_cchdo_param_names
             # are the names for one profile
 
-            included, excluded, included_param_argovis_names, excluded_argovis_param_names = add_argovis_vars_one_profile(
-                profile_dict)
+            (
+                included,
+                excluded,
+                included_param_argovis_names,
+                excluded_argovis_param_names,
+            ) = add_argovis_vars_one_profile(profile_dict)
 
             vars_included.extend(included)
             vars_excluded.extend(excluded)
@@ -164,21 +166,23 @@ def add_vars_one_cruise(data_type_profiles_objs):
 
 
 def gather_included_excluded_vars(batch_cruises_profiles_objs):
-
     cruises_all_included = []
     cruises_all_excluded = []
 
     for cruise_profiles_obj in batch_cruises_profiles_objs:
-
-        expocode = cruise_profiles_obj['cruise_expocode']
-        profiles_objs = cruise_profiles_obj['all_data_types_profile_objs']
+        expocode = cruise_profiles_obj["cruise_expocode"]
+        profiles_objs = cruise_profiles_obj["all_data_types_profile_objs"]
 
         # included and excluded are lists of tuples (name, profile_id, data_type)
 
         # unique_included_names are unique included names for all cruise
         # unique_excluded_names are unique excluded names for all cruise
-        included, excluded, unique_included_names, unique_excluded_names = add_vars_one_cruise(
-            profiles_objs)
+        (
+            included,
+            excluded,
+            unique_included_names,
+            unique_excluded_names,
+        ) = add_vars_one_cruise(profiles_objs)
 
         logging.info(f"expocode: {expocode}")
         logging.info(f"Num of included vars {len(unique_included_names)}")
