6403f22015aca1ba1a75ca43e7546c59a27740f4
Lynne Merchant <lmerchant@ucsd.edu>
2023-08-03 09:32:56 -0700

    Add save function for a single data type profile

--- check_and_save/save_output.py
+++ check_and_save/save_output.py
@@ -53,20 +53,6 @@ def get_unique_cchdo_units(data_type_profiles):
             else:
                 all_cchdo_units_mapping[cchdo_param_name] = cchdo_unit
 
-        # TODO
-        # overwrite key if it already exists
-        # each variable has same units. Is this true
-        # if it comes from either btl or ctd?
-        # If same key but different units, add suffix to key
-        # of profile_dict['meta']['expocode']
-        # expocode = profile_dict["meta"]["expocode"]
-        # for key, val in cchdo_units_mapping.items():
-        #     if key in all_cchdo_units_mapping and val != all_cchdo_units_mapping[key]:
-        #         new_key = f"{key}_{expocode}"
-        #         all_cchdo_units_mapping[new_key] = val
-        #     else:
-        #         all_cchdo_units_mapping[key] = val
-
         return all_cchdo_units_mapping
 
 
@@ -76,73 +62,6 @@ def write_all_cchdo_units(all_cchdo_units_mapping):
 
     with open(filepath, "a") as f:
         json.dump(all_cchdo_units_mapping, f, indent=4, sort_keys=True, default=convert)
-
-
-def write_profile_cchdo_units_one_profile(data_type, profile):
-    profile_dict = profile["profile_dict"]
-
-    filename = "found_cchdo_units.txt"
-    filepath = os.path.join(GlobalVars.LOGGING_DIR, filename)
-
-    # Write one profile cchdo units to
-    # keep a record of what units need to be converted
-
-    # cchdo names are the keys and argovis names are the value
-    key_mapping = get_program_argovis_source_info_mapping()
-
-    cchdo_units_key = "cchdo_units"
-    renamed_cchdo_units_key = key_mapping[cchdo_units_key]
-
-    cchdo_units_key_btl = "cchdo_units_btl"
-    renamed_cchdo_units_key_btl = key_mapping[cchdo_units_key_btl]
-
-    cchdo_units_key_ctd = "cchdo_units_ctd"
-    renamed_cchdo_units_key_ctd = key_mapping[cchdo_units_key_ctd]
-
-    if data_type == "btl":
-        cchdo_units_mapping = profile_dict[renamed_cchdo_units_key]
-
-    if data_type == "ctd":
-        cchdo_units_mapping = profile_dict[renamed_cchdo_units_key]
-
-    with open(filepath, "a") as f:
-        json.dump(cchdo_units_mapping, f, indent=4, sort_keys=True, default=convert)
-
-
-def write_profile_cchdo_units(checked_profiles_info):
-    # TODO
-    # Keep my names and map to latest Argovis names
-    # need this since already renamed individual files
-
-    # cchdo names are the keys and argovis names are the value
-    key_mapping = get_program_argovis_source_info_mapping()
-
-    cchdo_units_key = "cchdo_units"
-    renamed_cchdo_units_key = key_mapping[cchdo_units_key]
-
-    # Write one profile cchdo units to
-    # keep a record of what units need to be converted
-
-    try:
-        profile_dict = checked_profiles_info[0]["profile_checked"]["profile_dict"]
-
-        data_type = profile_dict["data_type"]
-
-        filename = "found_cchdo_units.txt"
-        filepath = os.path.join(GlobalVars.LOGGING_DIR, filename)
-
-        if data_type == "btl":
-            cchdo_units_profile = profile_dict[renamed_cchdo_units_key]
-
-        if data_type == "ctd":
-            cchdo_units_profile = profile_dict[renamed_cchdo_units_key]
-
-        with open(filepath, "a") as f:
-            json.dump(cchdo_units_profile, f, indent=4, sort_keys=True, default=convert)
-
-    except KeyError:
-        # Skip writing file
-        pass
 
 
 def save_included_excluded_cchdo_vars(included, excluded):
@@ -183,35 +102,6 @@ def save_included_excluded_cchdo_vars(included, excluded):
                 f.write(f"{id}\n")
 
 
-# def save_data_type_profiles_new(all_profiles):
-
-#     logging.info('Saving files')
-
-#     # Loop through all profiles, get all units and get unique
-#     all_cchdo_units_mapping = get_unique_cchdo_units(all_profiles)
-
-#     write_all_cchdo_units(all_cchdo_units_mapping)
-
-#     save_as_zip_data_type_profiles(all_profiles)
-
-
-# def save_data_type_profiles2(all_profiles_objs):
-
-#     logging.info('Saving files')
-
-#     for profiles_obj in all_profiles_objs:
-
-#         data_type = profiles_obj['data_type']
-#         all_profiles = profiles_obj['data_type_profiles_list']
-
-#         # Loop through all profiles, get all units and get unique
-#         all_cchdo_units_mapping = get_unique_cchdo_units(all_profiles)
-
-#         write_all_cchdo_units(all_cchdo_units_mapping)
-
-#         save_as_zip_data_type_profiles(all_profiles)
-
-
 def save_data_type_profiles(all_profiles):
     logging.info("Saving files")
 
@@ -225,3 +115,22 @@ def save_data_type_profiles(all_profiles):
     # each profile
 
     save_as_zip_data_type_profiles(all_profiles)
+
+
+def save_cruise_objs_by_type(cruise_objs_by_type):
+    for cruise_obj in cruise_objs_by_type:
+        expocode = cruise_obj["cruise_expocode"]
+
+        # Save expocode processed to a file collecting all processed
+        processed_cruises_file = (
+            Path(GlobalVars.LOGGING_DIR) / "all_cruises_processed.txt"
+        )
+        with open(processed_cruises_file, "a") as f:
+            f.write(f"{expocode}\n")
+
+        all_data_types_profile_objs = cruise_obj["all_data_types_profile_objs"]
+
+        for data_types_profile_obj in all_data_types_profile_objs:
+            data_type_profile_list = data_types_profile_obj["data_type_profiles_list"]
+
+            save_data_type_profiles(data_type_profile_list)
