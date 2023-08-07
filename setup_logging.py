d0a7a1d277437f3dbd69e371787243e38bc4daa0
Lynne Merchant <lmerchant@ucsd.edu>
2023-08-06 23:17:45 -0700

    Add logging file

--- setup_logging.py
+++ setup_logging.py
@@ -8,18 +8,20 @@ from global_vars import GlobalVars
 
 
 def setup_logger():
-
     filename = GlobalVars.OUTPUT_LOG
     logging_path = os.path.join(GlobalVars.LOGGING_DIR, filename)
     logging.root.handlers = []
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
-                        level=logging.INFO, filename=logging_path)
+    logging.basicConfig(
+        format="%(asctime)s : %(levelname)s : %(message)s",
+        level=logging.INFO,
+        filename=logging_path,
+    )
 
     # set up logging to console
     console = logging.StreamHandler()
     console.setLevel(logging.INFO)
     # set a format which is simpler for console use
-    formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(message)s')
+    formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(message)s")
     console.setFormatter(formatter)
     logging.getLogger("").addHandler(console)
 
@@ -35,27 +37,27 @@ def remove_file(filename, dir):
 
 
 def delete_logs(append_logs):
-
     logging_dir = GlobalVars.LOGGING_DIR
 
     if not append_logs:
         remove_file(GlobalVars.OUTPUT_LOG, logging_dir)
-        remove_file('file_read_errors.txt', logging_dir)
-        remove_file('found_cchdo_units.txt', logging_dir)
-        remove_file('cruises_no_core_ctd_vars.txt', logging_dir)
-        remove_file('cruises_w_ctd_temp_no_qc.txt', logging_dir)
-        remove_file('cruises_w_ctd_temp_no_ref_scale.txt', logging_dir)
-        remove_file('cruise_files_no_ctd_temp.txt', logging_dir)
-        remove_file('cruises_no_expocode.txt', logging_dir)
-        remove_file('cruise_files_no_pressure.txt', logging_dir)
-        remove_file('found_cruises_with_coords_netcdf.txt', logging_dir)
-        remove_file('diff_cruise_and_file_expocodes.txt', logging_dir)
-        remove_file('cruise_files_not_converted.txt', logging_dir)
-        remove_file('cruises_w_ctd_temp_unk.txt', logging_dir)
-        remove_file('profiles_information.txt', logging_dir)
-        remove_file('all_cruises_not_processed.txt', logging_dir)
-        remove_file('all_cruises_processed.txt', logging_dir)
-        remove_file('cruise_files_multiple_ctd_temp_oxy.txt', logging_dir)
+        remove_file("file_read_errors.txt", logging_dir)
+        remove_file("found_cchdo_units.txt", logging_dir)
+        remove_file("cruises_no_core_ctd_vars.txt", logging_dir)
+        remove_file("cruises_w_ctd_temp_no_qc.txt", logging_dir)
+        remove_file("cruises_w_ctd_temp_no_ref_scale.txt", logging_dir)
+        remove_file("cruise_files_no_ctd_temp.txt", logging_dir)
+        remove_file("cruises_no_expocode.txt", logging_dir)
+        remove_file("cruise_files_no_pressure.txt", logging_dir)
+        remove_file("found_cruises_with_coords_netcdf.txt", logging_dir)
+        remove_file("diff_cruise_and_file_expocodes.txt", logging_dir)
+        remove_file("cruise_files_not_converted.txt", logging_dir)
+        remove_file("cruises_w_ctd_temp_unk.txt", logging_dir)
+        remove_file("profiles_information.txt", logging_dir)
+        remove_file("all_cruises_not_processed.txt", logging_dir)
+        remove_file("all_cruises_processed.txt", logging_dir)
+        remove_file("cruise_files_multiple_ctd_temp_oxy.txt", logging_dir)
+        remove_file("all_included_params.txt", logging_dir)
 
     include_exclude_dir = GlobalVars.INCLUDE_EXCLUDE_DIR
 
@@ -63,7 +65,6 @@ def delete_logs(append_logs):
 
 
 def setup_logging(append_logs):
-
     os.makedirs(GlobalVars.LOGGING_DIR, exist_ok=True)
     os.makedirs(GlobalVars.INCLUDE_EXCLUDE_DIR, exist_ok=True)
 
